e35b7e0aab1eba86d697cc7e2c18f5e735d3c082
JonatanBerg02 <54435617+JonatanBerg02@users.noreply.github.com>
2020-09-09 09:15:15 +0200

    Did a thing
    Skrev mer

---
+++ Arv/Rectangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Rectangle : Shape
+    {
+        public Rectangle(int height, int width) : base(height, width) { }
+        public int RecArea()
+        {
+            int recArea = Height * width;
+            return recArea;
+
+        }
+       public int RecCircumferance()
+        {
+            int recCircumferance = Height * 2 + width * 2;
+            return recCircumferance;
+        }
+
+    }
+}
---
+++ Arv/Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template
+{
+    class Triangle : Shape
+    {
+        public Triangle(int height, int width) : base(height, width)
+        {
+                
+        }
+
+        public Area()
+        {
+               
+        }
+
+        public Circumferance()
+        {
+
+        }
+    }
+}
